eada49b90ba441743f270f6fea8e2663c710ae96
wilbere <elementaltcode@gmail.com>
2019-03-14 03:49:08 -0430

    envio de email

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,10 @@ use DB;
 use App\User;
 use App\Empresa;
 use Auth;
+use Faker\Generator as Faker;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 
 class UsersController extends Controller
 {
@@ -30,7 +34,7 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function crearusuario(Request $request)
+    public function crearusuario(Request $request,  Faker $faker)
     {
         $validatedData = $request->validate([
             'name' => 'string|required',
@@ -73,6 +77,8 @@ class UsersController extends Controller
 
         }else{ #Si no hay coincidencias
 
+            $password = $faker->bothify('???#?#??'); ## CREAR CONTRASE#A ALEATORIA
+
             $user = new User();
 
             if ($request->file('profile')) {
@@ -88,16 +94,62 @@ class UsersController extends Controller
             $user->birthdate = $request->birthdate;
             $user->model = "natural";
             $user->address =$request->address;
-            $user->password = Hash::make('123456');
+            $user->password = Hash::make($password);
             $user->nationality = $request->nationality;
             $user->save();
             #Creo el nuevo usuario
 
-            ##
-            ##  FALTA GUARDAR EL CARGO Y PEGARLO A LA EMPRESA
-            ##
+            DB::table('empresa_user')->insert([
+                'user_id' => $user->id,         ##
+                'empresa_id' => $empresaID,     ##  CREO LA RELACION
+                'cargo' => $request->cargo,     ##
+                'estado' => 'activo'
+            ]);
 
-            dd($user);
+            ## AHORA, ENVIAR CORREO CON SU PASSWORD
+            $mail = new PHPMailer(true);                            // Passing `true` enables exceptions
+  
+            try {
+                // Server settings
+                $mail->SMTPDebug = 0;                                	// Enable verbose debug output
+                $mail->isSMTP();                                     	// Set mailer to use SMTP
+                $mail->Host = 'smtp.gmail.com';												// Specify main and backup SMTP servers
+                $mail->SMTPAuth = true;                              	// Enable SMTP authentication
+                $mail->Username = 'your-email@gmail.com'; #Cambiar            // SMTP username
+                $mail->Password = 'your-gmail-password';  #Cambiar            // SMTP password
+                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = 587;                                    // TCP port to connect to
+
+                //Recipients
+                $mail->setFrom($user->email, $user->name);  ## DESTINATARIO
+                #$mail->addAddress('his-her-email@gmail.com', 'Optional name');	// Add a recipient, Name is optional
+                #$mail->addReplyTo('your-email@gmail.com', 'Mailer');
+                #$mail->addCC('his-her-email@gmail.com');
+                #$mail->addBCC('his-her-email@gmail.com');
+
+                //Attachments (optional)
+                // $mail->addAttachment('/var/tmp/file.tar.gz');			// Add attachments
+                // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');	// Optional name
+
+                $html = file_get_contents(asset('plantillas/email.html')); ## SE CARGA LA PLANTILLA
+                $html2 = str_replace('EMPRESA',$empresa->name, $html); ## SE CAMBIA EL NOMBRE DE LA EMPRESA
+                $html3 = str_replace('CONTACTO',$user->name, $html2); ## SE CAMBIA EL NOMBRE DE EL USUARIO
+                $htmlf = str_replace('PASSWORD',$password, $html3); ## SE INGRESA LA PASSWORD
+
+                //Content
+                $mail->isHTML(true); 					// Set email format to HTML
+                $mail->Subject = "Welcome to Vitalics";
+                $mail->Body    = $htmlf;						// message
+
+                $mail->send();
+                return back()->with('message','Usuario registrado exitosamente');
+
+            } catch (Exception $e) {
+
+                return back()->with('error','Message could not be sent.');
+
+            }
+            
 
         }
 
