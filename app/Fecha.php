d62e9a2383218e9eb27eb45eb6c6293a52f64d62
eJme1992 <edwin.jme@gmail.com>
2019-03-27 18:58:06 -0400

    actualizacion

---
+++ app/Fecha.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fecha extends Model
+{
+    //
+}
---
+++ database/migrations/2019_03_27_224751_create_fechas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFechasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fechas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('seccion_id')->unsigned()->nullable(); 
+            $table->string('hora');
+            $table->string('fecha');       
+            $table->timestamps();       
+            $table->foreign('seccion_id')->references('id')->on('seccion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fechas');
+    }
+}
