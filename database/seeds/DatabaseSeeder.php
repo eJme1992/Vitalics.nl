bbe2b8d1dbce288120e281fa1baf5d09237be0b5
Francisco Hernandez <francisco20990@gmail.com>
2019-04-07 11:26:08 -0400

    seeder de points

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+         App\StorePoint::create([
+          'price'   => '1',
+          'currency'     => 'EUR'
+          
+        ]);
     }
 }
